54e6880fb6840b15ace0f0af4c623f9452bb6aff
Diego Parra <Diegod2694@gmail.com>
2019-06-02 20:07:09 -0500

    mock data added

---
+++ src/mock-data/categories.js
@@ -0,0 +1,79 @@
+const categories = { categories: [
+	{
+		id: 11,
+		name: "Bebidas",
+		sublevels: [
+			{
+				id: 12,
+				name: "Gaseosas",
+				sublevels: [
+					{
+						id: 2,
+						name: "Con azúcar"
+					},
+					{
+						id: 3,
+						name: "Sin azúcar"
+					}
+				]
+			}
+		]
+	},
+	{
+		id: 12,
+		name: "Desayunos",
+		sublevels: [
+			{
+				id: 24,
+				name: "Fake 1",
+				sublevels: [
+					{
+						id: 5,
+						name: "Fake 2"
+					},
+					{
+						id: 36,
+						name: "Fake 3",
+						sublevels: [
+							{
+								id: 7,
+								name: "Fake 4"
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		id: 18,
+		name: "Almuerzos",
+		sublevels: [
+			{
+				id: 9,
+				name: "Fake 5"
+			},
+			{
+				id: 10,
+				name: "Fake 6"
+			}
+		]
+	},
+	{
+		id: 111,
+		name: "Vinos",
+		sublevels: [
+			{
+				id: 12,
+				name: "Fake 8"
+			},
+			{
+				id: 13,
+				name: "Fake 9"
+			}
+		]
+	}
+]
+}
+
+export default categories
